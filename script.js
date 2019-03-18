5f0ccd4daa8005352384ccad0efca160a6d0194f
NoahRoxs <43574943+NoahRoxs@users.noreply.github.com>
2019-03-18 13:54:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+class Circle {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(){
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.fillStyle = "aliceblue";
+    context.lineWidth = "4";
+    context.arc(this.x,this.y,50,0,Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+  }
+}
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let myCircle = new Circle(200,300);
+
+let v_x = 300;
+
+function getRandomNumber(max){
+  return Math.floor(Math.random()*max);
+}
+
+function animate(){
+  context.clearRect(0,0,canvas.width,canvas.height)
+  requestAnimationFrame(animate);
+  myCircle.x += v_x;
+  myCircle.draw();
+
+  if(myCircle.x >= canvas.width){
+    v_x = - v_x;
+  }
+  if(myCircle.x <= 0){
+    v_x = - v_x;
+  }
+}
+
+animate();
